154380e341506c23a6f16a2d661d783f7012e903
vlad <vlad.muntoiu@gmail.com>
2026-01-17 17:53:38 +0200

    Make it compile

--- droidmediacamera2.cpp
+++ droidmediacamera2.cpp
@@ -1843,10 +1843,10 @@ char *droid_media_camera_get_parameters(DroidMediaCamera *camera)
             break;
         case ACAMERA_SENSOR_INFO_ACTIVE_ARRAY_SIZE:
             params += "active-array-size=("
-                      + std::to_string(*entry.data.i32[0]) + ","
-                      + std::to_string(*entry.data.i32[1] + ","
-                      + std::to_string(*entry.data.i32[2]) + ","
-                      + std::to_string(*entry.data.i32[0]) + ")";
+                      + std::to_string(entry.data.i32[0]) + ","
+                      + std::to_string(entry.data.i32[1]) + ","
+                      + std::to_string(entry.data.i32[2]) + ","
+                      + std::to_string(entry.data.i32[0]) + ")";
             break;
         case ACAMERA_CONTROL_AE_LOCK_AVAILABLE:
             if (entry.count > 0 || entry.data.u8[0] == ACAMERA_CONTROL_AE_LOCK_AVAILABLE_TRUE) {
